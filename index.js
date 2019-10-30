5046ca18d07a6a28541621e1826ba175d5556d6a
Leelsmuth <femianjorin@rocketmail.com>
2019-10-30 16:46:06 +0100

    wip

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 const server = require("./server");
 
 server.listen(4500, () => {
-  console.log("\n* Server Running on http://localhost:4000 *\n");
+  console.log("\n* Server Running on http://localhost:4500 *\n");
 });
--- users/userRouter.js
+++ users/userRouter.js
@@ -4,45 +4,119 @@ const users = require("./userDb");
 
 const router = express.Router();
 
-router.post("/", (req, res) => {});
-
-router.post("/:id/posts", (req, res) => {});
-
-router.get("/", (req, res) => {});
-
-router.get("/:id", (req, res) => {});
-
-router.get("/:id/posts", (req, res) => {});
-
-router.delete("/:id", (req, res) => {});
-
-router.put("/:id", (req, res) => {});
-
 //custom middleware
+function validateUser(req, res, next) {
+  let user = req.body;
+
+  if (!user) {
+    res.status(400).json({ message: "missing user data" });
+  } else if (!user.name) {
+    res.status(400).json({ message: "missing required name field" });
+  } else {
+    next();
+  }
+}
 
 function validateUserId(req, res, next) {
-    users.getById(req.params.id)
+  users
+    .getById(req.params.id)
     .then(user => {
       if (user) {
         req.user = user;
-        next()
+        next();
       } else {
-        res.status(404).json({ message: 'user id does not correspond with an actual user' });
+        res.status(400).json({ message: "invalid user id" });
       }
     })
     .catch(error => {
       res.status(500).json({
-        message: 'Something terrible happend while checking user id: ' + error.message,
+        message:
+          "Something terrible happend while checking user id: " + error.message
       });
     });
 }
 
-function validateUser(req, res, next) {
-    let user = req.body
+function validatePost(req, res, next) {
+  let post = req.body;
 
-    
+  if (!post) {
+    res.status(400).json({ message: "missing post data" });
+  } else if (!post.text) {
+    res.status(400).json({ message: "missing required text field" });
+  } else {
+    next();
+  }
 }
 
-function validatePost(req, res, next) {}
+//implement here
+router.post("/", validateUser, (req, res) => {
+  users
+    .insert(req.body)
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(error => {
+      // log error to server
+      console.log(error);
+      res.status(500).json({
+        message: "Error adding the user: " + error.message
+      });
+    });
+});
+
+// router.post("/:id/posts", validateUserId, (req, res) => {
+//   let { id } = req.params;
+//   let post = users.getUserPosts(id);
+// });
+
+router.get("/", (req, res) => {
+  users
+    .get(req.query)
+    .then(post => {
+      res.status(200).json(post);
+    })
+    .catch(error => {
+      // log error to server
+      console.log(error);
+      res.status(500).json({
+        message: "Error retrieving the hubs"
+      });
+    });
+});
+
+router.get("/:id", validateUserId, (req, res) => {
+  res.json(req.user);
+});
+
+// router.get("/:id/posts", (req, res) => {});
+
+router.delete("/:id", validateUserId, (req, res) => {
+  users
+    .remove(req.user.id)
+    .then(() => {
+      // throw new Error('artifitial error while deleting!');
+      res.status(200).json({ message: "This user has been deleted" });
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: `Error deleting the user: ${error.message}`
+      });
+    });
+});
+
+router.put("/:id", validateUserId, (req, res) => {
+  users
+    .update(req.user.id, req.body)
+    .then(user => {
+      res.status(200).json({
+        message: "You have just changed his destiny"
+      });
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: "Error updating the user: " + error.message
+      });
+    });
+});
 
 module.exports = router;
