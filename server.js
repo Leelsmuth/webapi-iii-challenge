82d27ccf387bb5d282d28fb67da0617d0924db47
Leelsmuth <femianjorin@rocketmail.com>
2019-10-30 15:08:52 +0100

    validate user ID middleware implemented

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require("express");
 
+const helmet = require('helmet');
+const userRouter = require('./users/userRouter');
+// const postRouter = require('./posts/postRouter')
+
 const server = express();
+
+server.use(helmet());
+
+server.use(express.json());
+
+server.use('/api/users', userRouter);
 
 server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const users = require("./userDb");
+
 const router = express.Router();
 
 router.post("/", (req, res) => {});
@@ -18,9 +20,28 @@ router.put("/:id", (req, res) => {});
 
 //custom middleware
 
-function validateUserId(req, res, next) {}
+function validateUserId(req, res, next) {
+    users.getById(req.params.id)
+    .then(user => {
+      if (user) {
+        req.user = user;
+        next()
+      } else {
+        res.status(404).json({ message: 'user id does not correspond with an actual user' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: 'Something terrible happend while checking user id: ' + error.message,
+      });
+    });
+}
 
-function validateUser(req, res, next) {}
+function validateUser(req, res, next) {
+    let user = req.body
+
+    
+}
 
 function validatePost(req, res, next) {}
 
